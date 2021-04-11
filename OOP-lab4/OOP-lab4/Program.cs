86941ec1be95c658bf4260bc3cccce8bf0307f28
Diana <57933812+kalilask4@users.noreply.github.com>
2021-04-11 20:20:20 +0300

    Start lab4
    Start lab4

---
+++ OOP-lab4/OOP-lab4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
