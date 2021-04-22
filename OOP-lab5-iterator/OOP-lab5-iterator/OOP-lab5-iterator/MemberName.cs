baed5455a3efdd46cd764c636060e3639358b812
kalilask4 <kalilask4@gmail.com>
2021-04-22 17:54:54 +0300

    iterator added

---
+++ OOP-lab5-iterator/OOP-lab5-iterator/OOP-lab5-iterator/MemberName.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lab5_iterator
+{
+    class MemberName
+    {
+        int end;
+        string[] memberNames =
+        {
+            "BNikita",
+            "GSamanta",
+            "GVadim",
+            "JNatalia",
+            "JAndrey",
+            "ZVladimir",
+            "KAndrey",
+            "Kvladimir",
+            "KNadegda",
+            "KVitaliy",
+            "11"
+        };
+
+        public MemberName()
+        {
+            this.end = 21;
+        }
+
+        public IEnumerable MyMemberName(int begin, int end)
+        {
+            for (int i=begin; i <= end; i++)
+                {
+
+                if (i < 1 || i > memberNames.Length)
+                        yield break;
+                yield return (string)(memberNames[i - 1]);
+
+            }
+        }
+
+
+    }
+}
---
+++ OOP-lab5-iterator/OOP-lab5-iterator/OOP-lab5-iterator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP_lab5_iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemberName memberNames = new MemberName();
+
+            foreach (string s in memberNames.MyMemberName(1,5))
+            {
+                Console.WriteLine(s + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
