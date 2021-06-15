5f3641446d77971b301028b41be6ab21a0929ad5
kalilask4 <kalilask4@gmail.com>
2021-06-15 05:11:49 +0300

    work with queue, add some methods

--- OOP-lab6-param-coll/OOP-lab6-param-coll/OOP-lab6-param-coll/Helper.cs
+++ OOP-lab6-param-coll/OOP-lab6-param-coll/OOP-lab6-param-coll/Helper.cs
@@ -10,7 +10,7 @@ namespace OOP_lab6_param_coll
 {
     class Helper
     {
-        public static void launch_menu(ArrayList arrayList)
+        public static void launch_menu(Queue queue)
         {
             while (true)
             {
@@ -18,8 +18,9 @@ namespace OOP_lab6_param_coll
                 Console.WriteLine("*     Главное меню         *");
                 Console.WriteLine("****************************");
                 Console.WriteLine("1 – просмотр коллекции");
-                Console.WriteLine("2 – добавление элемента");
-                Console.WriteLine("3 – добавление элемента по указанному индексу");
+                Console.WriteLine("2 – добавление элемента в конец");
+                Console.WriteLine("3 – показать первый объект в очереди");
+                Console.WriteLine("4 – показать(обработать) первый объект в очереди и удалить");
                 Console.WriteLine("4 – нахождение элемента с начала коллекции");
                 Console.WriteLine("5 – нахождение элемента с конца коллекции");
                 Console.WriteLine("6 – удаление элемента по индексу");
@@ -31,22 +32,82 @@ namespace OOP_lab6_param_coll
                 int otvet = Convert.ToInt32(Console.ReadLine());
                 switch (otvet)
                 {
-                   /* case 1: Show(arrayList); break;
-                    case 2: Add(arrayList); break;
-                    case 3: Insert(arrayList); break;
-                    case 4: Find_start(arrayList); break;
-                    case 5: Find_end(arrayList); break;
-                    case 6: Del(arrayList); break;
-                    case 7: Del_on_value(arrayList); break;
-                    case 8: Revers(arrayList); break;
-                    case 9: Sort(arrayList); break;
-                    case 10: Glove(arrayList); break;*/
+                    case 1: Show(queue); break;
+                    case 2: Add(queue); break;
+                    case 3: Show_First(queue); break;
+                    case 4: Process_First(queue); break;
                     case 0: return;
                     default: return;
                 }
             }
         }
 
+       
+        public static void Show(Queue queue)
+        {
+            foreach (object obj in queue)
+            {
+                Console.WriteLine((obj as Star)?.ToString());
+            }
+            Console.ReadKey();
+
+        }
+
+
+
+        public static void Add(Queue queue)
+        {
+            Console.WriteLine("Добавляем Star(1) или NeutronStar(2)?");
+            string answer = Console.ReadLine();
+            int n;
+            if (!Int32.TryParse(answer, out n) || n < 1 || n > 2)
+                return;
+            Console.Write("name: ");
+            string name = Console.ReadLine();
+            Console.Write("temperature_K: ");
+            double temperature_K = double.Parse(Console.ReadLine());
+            Console.Write("sun_mass: ");
+            double sun_mass = double.Parse(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    queue.Enqueue(new Star(name, temperature_K, sun_mass));
+                    break;
+                case 2:
+                    queue.Enqueue(new NeutronStar(name, temperature_K, sun_mass));
+                    break;
+            }
+        }
+
+        public static void Show_First(Queue queue)
+        {
+            Console.Write("Первый в очереди - ");
+            Console.WriteLine((queue.Peek() as Star)?.ToString());
+            Console.ReadKey();
+
+        }
+
+        public static void Process_First(Queue queue)
+        {
+            string answer;
+            int n=1;
+
+            while (n == 1)
+            {
+
+                Console.Write("Первый в очереди - ");
+                Console.WriteLine((queue.Dequeue() as Star)?.ToString());
+                Console.WriteLine("Обработан, удален");
+                Console.WriteLine("Продолжить (1) или закончить(0)?");
+
+                answer = Console.ReadLine();
+                if (!int.TryParse(answer, out n))
+                    return;
+
+            }
+
+        }
+        
 
         public static void Imitate_loading(string str)
         {
--- OOP-lab6-param-coll/OOP-lab6-param-coll/OOP-lab6-param-coll/Program.cs
+++ OOP-lab6-param-coll/OOP-lab6-param-coll/OOP-lab6-param-coll/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace OOP_lab6_param_coll
 {
@@ -12,8 +13,20 @@ namespace OOP_lab6_param_coll
             Console.WriteLine("             ....................................\n");
 
             Helper.Imitate_loading(" loading   .........");
-          
 
+            Star star1 = new Star("Sun", 5778, 1);
+            Star star2 = new Star("Naos", 42000, 40, 19643814, 1076, "Zeta Puppis is a blue star in the constellation of Puppis.");
+            NeutronStar neutronStar1 = new NeutronStar("Alma Star", 17, 111, 8, 2663568, 168000, "");
+            Star star3 = new Star("Ann", 15233, 1.3);
+
+            Queue stars = new Queue();
+            stars.Enqueue(star1); 
+            stars.Enqueue(star2); 
+            stars.Enqueue(star3); 
+            stars.Enqueue(neutronStar1); 
+            stars.Enqueue(star1);
+
+            Helper.launch_menu(stars);
 
         }
     }
