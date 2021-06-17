8a346330b5a35f28ec90977ed70b8a817cd32073
kalilask4 <kalilask4@gmail.com>
2021-06-17 03:48:53 +0300

    add some metods. task solved

--- OOP-lab7-menu/OOP-lab6-menu/OOP-lab6-menu/Program.cs
+++ OOP-lab7-menu/OOP-lab6-menu/OOP-lab6-menu/Program.cs
@@ -127,11 +127,39 @@ namespace OOP_lab6_menu
         }
         static void f5(DirectoryInfo d)
         {// создание каталога в текущем
+            Console.WriteLine("Введите имя каталога ");
+            string str = Console.ReadLine();
+            if (Directory.Exists(str))
+            {
+                Console.WriteLine($"Каталог {str} уже существует");
+                return;
+            }
+            d.CreateSubdirectory(str);
+            Console.WriteLine($"Каталог {str} создан");
         }
         static void f6(DirectoryInfo d)
         {// удаление каталога по номеру, если он пустой
-
+            f2(d);
+            DirectoryInfo[] directories = d.GetDirectories();
+            Console.WriteLine("Ведите номер каталога для удаления");
+            int index = Convert.ToInt32(Console.ReadLine());
+            if(index < 0 || index >= directories.Length)
+            {
+                Console.WriteLine("Неверный номер каталога");
+                return;
+            }
+            if(directories[index].GetDirectories().Length == 0 && directories[index].GetFiles().Length == 0)
+            {
+                directories[index].Delete();
+                Console.WriteLine($"Каталог удален");
+            }
+            else
+            {
+                Console.WriteLine($"Каталог не пуст") ;
+            }
+ 
         }
+
         static void f7(DirectoryInfo d)
         {// удаление файлов с указанными номерами
             f3(d);
@@ -150,10 +178,19 @@ namespace OOP_lab6_menu
             Console.WriteLine(index2 - index1 + 1 + " файлов удалено");
             f3(d);
         }
+
         static void f8(DirectoryInfo d)
         {// вывод списка всех файлов с указанной датой создания (ищет в текущем каталоге и подкаталогах)
-
+            Console.WriteLine("Введите дату создания файла в формате ДД.ММ.ГГГГ");
+            DateTime date = Convert.ToDateTime(Console.ReadLine());
+            FileInfo[] files = d.GetFiles("*", SearchOption.AllDirectories);
+            for (int i=0; i< files.Length; i++)
+            {
+                if(files[i].CreationTime.Date.Equals(date.Date))
+                    Console.WriteLine($"{i} {files[i].Name} {files[i].CreationTime}");
+            }
         }
+
         static void f9(DirectoryInfo d)
         {// вывод списка всех текстовых файлов, в которых содержится указанный текст (ищет в текущем каталоге и подкаталогах)
             Console.WriteLine("Введите текст для поиска в файлах");
