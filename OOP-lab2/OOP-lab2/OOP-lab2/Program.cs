2013a3d95cc0a276bf32f1c68650fcd6e6e7335f
kalilask4 <kalilask4@gmail.com>
2021-07-05 01:53:52 +0300

    exception handling

--- OOP-lab2/OOP-lab2/OOP-lab2/Program.cs
+++ OOP-lab2/OOP-lab2/OOP-lab2/Program.cs
@@ -8,6 +8,27 @@ namespace OOP_lab2
         {
             Rational r1 = new Rational(1, 2);
             Console.WriteLine($"r1={r1}");
+            try
+            {
+
+                //Rational r2 = new Rational(1, 0); //здесь будет обработано исключение (знаменатель !=0)
+                //Console.WriteLine($"r2={r2}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"исключение - {e.Message}");
+            }
+            try
+            {
+                Rational r3 = new Rational(0, 1); //здесь будет обработано исключение (числитель !=0)
+                Console.WriteLine($"r3={r3}");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"исключение - {e.Message}");
+            }
+
         }
     }
 }
